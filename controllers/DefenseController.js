b1ed87d9246b1492c235c20a7bfda7ddcfe0b696
NadyaNIS <nadyaimanthie@gmail.com>
2021-01-10 12:24:38 +0530

    Permission

--- controllers/DefenseController.js
+++ controllers/DefenseController.js
@@ -10,7 +10,7 @@ exports.locationCheck = (req, res) => {
   const token = req.headers.auth;
   try {
     const decoded = jwt.verify(token, publicKey);
-    if (decoded.privileges.indexOf('ASSIGN_HERO') >= 0) {
+    if (decoded.privileges.indexOf('ACCESS_DEFENSE') >= 0 || decoded.privileges.indexOf('ADD_TO_CONSTELLATION') >= 0 || decoded.privileges.indexOf('MANEUVER_SATELLITE') >= 0) {
       Satellite.find({
         "settled_location.lat": req.params.lat,
         "settled_location.long": req.params.long
